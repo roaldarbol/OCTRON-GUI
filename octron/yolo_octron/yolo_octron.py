0f9a7124700569d125dd3feb3041be0928de28e0
roaldarbol <25629697+roaldarbol@users.noreply.github.com>
2026-03-18 12:07:20 +0100

    Don't overwrite by default and add timing of prediction

--- octron/yolo_octron/yolo_octron.py
+++ octron/yolo_octron/yolo_octron.py
@@ -1788,7 +1788,7 @@ class YOLO_octron:
                   iou_thresh=.7,
                   conf_thresh=.5,
                   opening_radius=0,
-                  overwrite=True,
+                  overwrite=False,
                   buffer_size=500,
                   ):
         """
@@ -1995,7 +1995,7 @@ class YOLO_octron:
             if save_dir.exists() and overwrite:
                 shutil.rmtree(save_dir)
             elif save_dir.exists() and not overwrite:
-                logger.info(f"Prediction directory already exists at {save_dir}")
+                logger.info(f"Skipping {video_name}: predictions already exist at {save_dir}. Pass --overwrite to replace.")
                 yield {
                     'stage': 'skipped_video',
                     'video_name': video_name,
